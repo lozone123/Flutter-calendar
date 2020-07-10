42c48676866a8386432ceba529ca7c1496ff31c9
623875158@qq.com <qq6582562>
2020-07-10 08:04:20 +0800

    删除

--- beauty_calendar_config.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class BeatutyCalendarConfig{
-   bool isShowLunar=true; //是否显示农历，true显示
-   int showMonth=3; //显示多少个月
-   Color curDateBgColor=Color(0xff00CAE2); //今天日期的背景颜色
-   Color curDateTextColor=Color(0xffffffff); //今天日期的文字颜色
-   Color bothSideTextColor=Color(0xff00CAE2); //两边的文字颜色值
-}
